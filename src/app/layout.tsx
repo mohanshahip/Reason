9df6e8b1aae1415333ffda8236bc5f0ac2421bac
mohan <mohanshahi.p@gmail.com>
2026-04-16 17:43:25 +0545

    fix metadata for vercel build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -102,7 +102,10 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.svg", type: "image/svg+xml" },
+    ],
     shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
